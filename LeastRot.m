bcefd6c71eec5d826d4df4f0cceb4875d0032a2e
mradhub <mlrad5755@gmail.com>
2012-06-28 19:18:32 -0400

    LeastRot.m preforms a rotation diagram analysis on the matched peaks

---
+++ LeastRot.m
@@ -0,0 +1,34 @@
+%function RotSim = LeastRot(NTL, NTU, FWHML, FWHMU, tempL, tempU, shiftL, shiftU, molFreq, obsFreq, obsInt, AguList, A, EUJ, threshold)
+% Input: NTL - column density lower limit (cm^-2)
+%        NTU - column density upper limit (cm^-2)
+%        FWHML - full width half max lower limit (km/s)
+%        FWHMU - full width half max upper limit (km/s)
+%        tempL - temperature lower limit (K)
+%        tempU - temperature uppper limit (K)
+%        shiftL - shift lower limit (MHz)
+%        shiftU - shift upper limit (MHz)
+%        molFreq - molecule frequency list from catalog file
+%        obsFreq - observational frequency data
+%        obsInt - observational intensity data
+%        AguList - Einstein A times upper state degeneracy for transitions
+%        A - coefficient of partition function fit
+%        EUJ - upper state energy in joules
+%        threshold - 3-sigma noise threshold in observational data
+% Output:RotSim = [NTM,NTSig,TempM,TempSig,shiftM,shiftSig,FWHMM,FWHMSig];
+
+function RotSim = LeastRot(NTL, NTU, FWHML, FWHMU, tempL, tempU, shiftL, shiftU, molFreq, obsFreq, obsInt, AguList, A, EUJ)
+FitSim = LeastSquares(NTL,NTU,FWHML,FWHMU,tempL,tempU,shiftL,shiftU,molFreq,obsFreq,obsInt,AguList,A,EUJ);
+[Peaks, PeaksList] = PeakList(obsFreq,obsInt,threshold);
+Simulation = totalSim(molFreq,obsFreq,AguList,A,EUJ,FitSim(1),FitSim(7),FitSim(3),FitSim(5));
+[IDPeaks]=PeakLite(obsFreq,obsInt,molFreq,AguList,EUJ,Peaks(:,end),Simulation,25);
+k =  1.38065e-23;
+h = 6.62606E-34; 
+c = 2.9979E+8;
+const = (8*pi*k*1.064*FitSim(7)*1E8)/(h*c^2);
+lnstuff = ln((IDPeaks(:,3)*IDPeaks(:,1)*const)/IDPeaks(:,4));
+EUK = IDPeaks(:,5)/k;
+p = polyfit(EUK,lnstuff,1);
+Trot = -1/p(1);
+NT = exp(p(2))*A*Trot^(3/2);
+RotSim(1) = Trot;
+RotSim(2)  = NT;
